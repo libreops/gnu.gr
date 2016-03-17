531898cb013365c73ab0b50db73b5b64dc1411b0
Nikos Roussos <nikos@roussos.cc>
2016-03-17 20:43:42 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# gnu.gr
+
+gnu.gr is a public jabber/xmpp service.
+This is the source code of its [website](https://gnu.gr)
+
+## license
+
+This software is licensed under the [GNU AFFERO GENERAL PUBLIC LICENSE](https://www.gnu.org/licenses/agpl-3.0.html).
+For more information, read the [LICENSE](LICENSE) file.
---
+++ static/js/app.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    'use strict';
+
+    // fade in #back-top
+    $(function () {
+        $(window).scroll(function () {
+            if ($(this).scrollTop() > 200) {
+                $('#back-top').fadeIn();
+            } else {
+                $('#back-top').fadeOut();
+            }
+        });
+
+        // scroll body to 0px on click
+        $('#back-top a').click(function () {
+            $('body,html').animate({
+                scrollTop: 0
+            }, 800);
+            return false;
+        });
+    });
+});
