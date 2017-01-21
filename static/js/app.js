684da9749f2c90494f4111a6d117a7bff813bd09
Nikos Roussos <nikos@roussos.cc>
2017-01-21 18:40:55 +0200

    Add certificate fingerprint

--- static/js/app.js
+++ static/js/app.js
@@ -2,21 +2,22 @@ $(document).ready(function() {
     'use strict';
 
     // fade in #back-top
-    $(function () {
-        $(window).scroll(function () {
-            if ($(this).scrollTop() > 200) {
-                $('#back-top').fadeIn();
-            } else {
-                $('#back-top').fadeOut();
-            }
-        });
-
-        // scroll body to 0px on click
-        $('#back-top a').click(function () {
-            $('body,html').animate({
-                scrollTop: 0
-            }, 800);
-            return false;
-        });
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('#back-top').fadeIn();
+        } else {
+            $('#back-top').fadeOut();
+        }
     });
+
+    // scroll body to 0px on click
+    $('#back-top a').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 800);
+        return false;
+    });
+
+    // display certificate fingerprint
+    $('#fingerprint').load('fingerprint.html');
 });
